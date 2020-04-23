b08edbd0450945f2bf9ea3b37f05b23f1dbec912
mrbrunelli <matheus.brunelli@gmail.com>
2020-04-23 14:46:41 -0300

    Programação em Python para criar um servidor para o index.html

---
+++ build-dev/run.py
@@ -0,0 +1,26 @@
+import logging
+import http.server
+import getpass
+
+class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        logging.info("%s - - [%s] %s\n"% (
+            self.client_adress[0],
+            self.log_date_time_string(),
+            format%args
+        ))
+
+logging.basicConfig(
+    filename='/log/http-server.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('inicializando...')
+PORT = 8000
+
+httpd = socketserver.TCPServer(("", PORT), MyHTTPHandler)
+logging.info('escutando a porta: %s', PORT)
+logging.info('usuario: %s', getpass.getUser())
+httpd.serve_forever()
