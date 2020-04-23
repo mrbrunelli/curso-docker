ae5ee0cbc57846b6e692258aa65d3f82ee3bd9f2
mrbrunelli <matheus.brunelli@gmail.com>
2020-04-23 16:16:26 -0300

    Rodando servidor Python em um container, e acessando por outro.

--- build-dev/run.py
+++ build-dev/run.py
@@ -1,11 +1,12 @@
 import logging
 import http.server
+import socketserver
 import getpass
 
 class MyHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
         logging.info("%s - - [%s] %s\n"% (
-            self.client_adress[0],
+            self.client_address[0],
             self.log_date_time_string(),
             format%args
         ))
@@ -15,12 +16,11 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
 logging.getLogger().addHandler(logging.StreamHandler())
 logging.info('inicializando...')
 PORT = 8000
 
 httpd = socketserver.TCPServer(("", PORT), MyHTTPHandler)
 logging.info('escutando a porta: %s', PORT)
-logging.info('usuario: %s', getpass.getUser())
+logging.info('usuário: %s', getpass.getuser())
 httpd.serve_forever()
