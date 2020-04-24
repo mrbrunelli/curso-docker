cfa46066c8708742c9763a31970b7029ad5f1ee3
mrbrunelli <matheus.brunelli@gmail.com>
2020-04-23 21:10:02 -0300

    Ambiente WEB PHP & Apache & Mysql.

---
+++ php-apache-mysql/php/index.php
@@ -0,0 +1,27 @@
+<?php
+$timezones = array(
+    'SP' => 'America/Sao_Paulo',
+    'BA' => 'America/Bahia'
+);
+
+date_default_timezone_set($timezones['SP']);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP & Apache</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <h1>Hello World</h1>
+        <h2>Data de hoje: <?= date('d/m/Y H:i:s') ?></h2>
+    </div>
+</body>
+
+</html>
